389aafe43ef63bc6bc377b45ee8c3caad379b38f
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-03-27 17:30:34 +0100

    ssh: add partial option loader for kex and hostkey algos

--- src/main/java/me/laszloattilatoth/jada/proxy/ssh/Options.java
+++ src/main/java/me/laszloattilatoth/jada/proxy/ssh/Options.java
@@ -19,12 +19,51 @@ package me.laszloattilatoth.jada.proxy.ssh;
 import me.laszloattilatoth.jada.config.ProxyOptions;
 import me.laszloattilatoth.jada.proxy.ssh.core.Side;
 
+import java.util.Map;
+
 public class Options extends ProxyOptions {
     public final SideOptions clientSide = new SideOptions();
     public final SideOptions serverSide = new SideOptions();
 
     public final SideOptions sideOptions(Side side) {
         return side == Side.CLIENT ? clientSide : serverSide;
+    }
+
+    @Override
+    public void load(Map<String, Object> globalOptions, Map<String, Object> options) throws InvalidOptions {
+        if (globalOptions != null)
+            loadSingle(globalOptions);
+
+        if (options != null)
+            loadSingle(options);
+    }
+
+    private void loadSingle(Map<String, Object> options) throws InvalidOptions {
+        for (Map.Entry<String, Object> entry : options.entrySet()) {
+
+            SideOptions sideOptions = switch (entry.getKey()) {
+                case "client" -> clientSide;
+                case "server" -> serverSide;
+                default -> throw new InvalidOptions(String.format("Unknown SSH option  %s", entry.getKey()));
+            };
+            if (entry.getValue() instanceof Map entries) {
+                Map<String, Object> map = entries;
+                for (Map.Entry<String, Object> sideEntry : map.entrySet()) {
+                    processEntry(sideOptions, sideEntry);
+                }
+            }
+        }
+    }
+
+    private void processEntry(SideOptions sideOptions, Map.Entry<String, Object> entry) throws InvalidOptions {
+        String key = entry.getKey();
+        if (entry.getValue() instanceof String value) {
+            switch (key) {
+                case "kex-algos" -> sideOptions.kexAlgorithms = value;
+                case "hostkey-algos" -> sideOptions.hostkeyAlgorithms = value;
+                default -> throw new InvalidOptions(String.format("Unknown SSH option  %s", entry.getKey()));
+            }
+        }
     }
 
     public static class SideOptions {
