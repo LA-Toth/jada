1d6850dbc74b26358b955f62710b23d2e5e9bf62
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2026-02-25 23:24:12 +0100

    ssh: transport layer: improved packet name logging using predefined names

--- src/main/java/me/laszloattilatoth/jada/proxy/ssh/helpers/LoggerHelper.java
+++ src/main/java/me/laszloattilatoth/jada/proxy/ssh/helpers/LoggerHelper.java
@@ -17,10 +17,17 @@
 
 package me.laszloattilatoth.jada.proxy.ssh.helpers;
 
+import me.laszloattilatoth.jada.proxy.ssh.core.Constant;
+
 public class LoggerHelper {
+    public static final String NOT_DEFINED = "(not predefined)";
 
     public static String formatSideStr(boolean client_to_server) {
         return client_to_server ? "client->server" : "server->client";
+    }
+
+    public static String packetTypeName(int packetType) {
+        return Constant.SSH_MSG_NAMES[packetType] != null ? Constant.SSH_MSG_NAMES[packetType] : NOT_DEFINED;
     }
 
     private LoggerHelper() {
